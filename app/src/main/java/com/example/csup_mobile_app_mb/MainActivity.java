6e36d85f59e8aad4024e34c284adf7979c3c23f2
E1337K1 <eldin.kurpejovic@udg.edu.me>
2019-11-24 12:42:23 +0100

    First commit

---
+++ app/src/main/java/com/example/csup_mobile_app_mb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.csup_mobile_app_mb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
